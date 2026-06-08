c85ef5ce121cd0242853d3231a4f7ff563b8eaca
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-08 23:00:02 +0200

    ex 6 adding a book

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -122,6 +123,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]!
     allAuthors: [Author]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -143,6 +153,15 @@ const resolvers = {
   Author: {
     bookCount: (author) =>
       books.filter((book) => book.author === author.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      if (!authors.find((author) => author.name === args.author))
+        authors = authors.concat({ name: args.author, id: uuid() });
+      books = books.concat(book);
+      return book;
+    },
   },
 };
 
