c793becb46ac79710a261dddf02eed576172d405
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-26 22:38:30 +0200

    ex 8 authors view

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@apollo/client/react'
+import { ALL_AUTHORS } from '../queries'
+
 const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -14,7 +21,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {authors.data.allAuthors.map((a) => (
             <tr key={a.id}>
               <td>{a.name}</td>
               <td>{a.born}</td>
