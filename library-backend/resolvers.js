f9f16f4f2737f8e04603f7b9e82b3885cfcdbdc3
Pippa van Manen <pippavanmanen@icloud.com>
2026-07-10 19:57:34 +0200

    ex 16 user and logging in

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -1,10 +1,13 @@
 const { GraphQLError } = require('graphql')
 const { v4: uuid } = require('uuid')
+const jwt = require('jsonwebtoken')
 const Author = require('./models/author')
 const Book = require('./models/book')
+const User = require('./models/user')
 
 const resolvers = {
   Query: {
+    me: (root, args, { currentUser }) => currentUser,
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     allBooks: async (root, { author, genre }) => {
@@ -25,7 +28,46 @@ const resolvers = {
       1
   },
   Mutation: {
-    addBook: async (root, args) => {
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch(error => {
+          throw new GraphQLError(`Creating user failed: ${error.message}`, {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args,
+              error
+            },
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError(`wrong credentials`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('User not authenticated', {
+          extensions: {
+            code: 'UNAUTHENTICATED',
+          },
+        })
+      }
       const author = await Author.findOne({ name: args.author }) ||
         await new Author({ name: args.author}).save()
       const book = new Book({ ...args, id: uuid(), author })
@@ -42,7 +84,14 @@ const resolvers = {
       }
       return book.populate('author');
     },
-    editAuthor: async (root, { name, setBornTo }) => {
+    editAuthor: async (root, { name, setBornTo }, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('User not authenticated', {
+          extensions: {
+            code: 'UNAUTHENTICATED',
+          },
+        })
+      }
       const author = await Author.findOne({ name })
       if (!author) {
         throw new GraphQLError(`Author ${name} not found`, {
--- library-backend/schema.js
+++ library-backend/schema.js
@@ -13,11 +13,20 @@ const typeDefs = /* GraphQL */ `
     books: [Book]!
     bookCount: Int!
   }
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  type Token {
+    value: String!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book]!
     allAuthors: [Author]!
+    me: User
   }
 
   type Mutation {
@@ -31,6 +40,14 @@ const typeDefs = /* GraphQL */ `
       name: String!
       setBornTo: Int!
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
