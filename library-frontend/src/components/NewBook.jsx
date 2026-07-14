401c050a93448850cb4dbf46c7b51e51aded77b7
Pippa van Manen <pippavanmanen@icloud.com>
2026-07-14 17:35:31 +0200

    ex 23

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client/react'
-import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
+import { CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -10,7 +10,7 @@ const NewBook = (props) => {
   const [genres, setGenres] = useState([])
 
   const [createBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+    refetchQueries: ['allBooks', 'allAuthors']
   })
 
   if (!props.show) {
